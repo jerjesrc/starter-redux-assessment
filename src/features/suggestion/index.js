e7740c27e287408f5434a7aa496906d6f34e15be
Jerjes Romero Castaneda <jerjesr@indeed.com>
2024-02-08 18:09:56 -0800

    starter-redux-assessment

--- src/features/suggestion/index.js
+++ src/features/suggestion/index.js
@@ -4,24 +4,24 @@ import {
   fetchSuggestion,
   selectError,
   selectLoading,
+  selectSuggestion
   // Task 18: Import the `selectSuggestion()` selector from the suggestion slice
 } from './suggestion.slice';
 import './suggestion.css';
-
 export default function Suggestion() {
   // Task 19: Call useSelector() with the selectSuggestion() selector
   // The component needs to access the `imageUrl` and `caption` properties of the suggestion object.
+  const { imageUrl, caption } = useSelector(selectSuggestion);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
   const dispatch = useDispatch();
-
   useEffect(() => {
     async function loadSuggestion() {
       // Task 20: Dispatch the fetchSuggestion() action creator
+      await dispatch(fetchSuggestion());
     }
     loadSuggestion();
   }, [dispatch]);
-
   let render;
   if (loading) {
     render = <h3>Loading...</h3>;
@@ -30,17 +30,16 @@ export default function Suggestion() {
   } else {
     // Task 21: Enable the two JSX lines below needed to display the suggestion on the page
     render = (
-      <>
-        {/* <img alt={caption} src={imageUrl} />
-        <p>{imageUrl}</p> */}
-      </>
+        <>
+          <img alt={caption} src={imageUrl} />
+          <p>{imageUrl}</p>
+        </>
     );
   }
-
   return (
-    <section className="suggestion-container">
-      <h2>Suggestion of the Day</h2>
-      {render}
-    </section>
+      <section className="suggestion-container">
+        <h2>Suggestion of the Day</h2>
+        {render}
+      </section>
   );
 }
